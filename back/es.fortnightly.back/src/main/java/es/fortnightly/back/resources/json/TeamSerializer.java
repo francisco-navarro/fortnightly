cc775f78cf8b9e916b4d5362d52d8e2a8c01a4e5
Paco Navarro <franciscojose.navarro.contractor@bbva.com>
2016-07-02 14:18:01 +0200

    Arrancar el webapp

--- back/es.fortnightly.back/src/main/java/es/fortnightly/back/resources/json/TeamSerializer.java
+++ back/es.fortnightly.back/src/main/java/es/fortnightly/back/resources/json/TeamSerializer.java
@@ -7,44 +7,25 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
-public class TeamSerializer extends JsonSerializer<Item> {
+import es.fortnightly.back.resources.Team;
+
+public class TeamSerializer extends JsonSerializer<Team> {
 
 	@Override
-	public void serialize(Item item, JsonGenerator jgen, SerializerProvider provider)
+	public void serialize(Team team, JsonGenerator jgen, SerializerProvider provider)
 			throws IOException, JsonProcessingException {
 		jgen.writeStartObject();
-	    jgen.writeStringField("name", item.getDescription());
-	    jgen.writeArrayFieldStart("data");
-	    for(Value value : item.getData()){
-	    	jgen.writeStartObject();
-	    	writeElement(jgen, value); 
-	    	jgen.writeEndObject();
-	    }
+	    jgen.writeStringField("name", team.getNombre());
+//	    jgen.writeArrayFieldStart("data");
+//	    for(Value value : item.getData()){
+//	    	jgen.writeStartObject();
+//	    	writeElement(jgen, value); 
+//	    	jgen.writeEndObject();
+//	    }
 	    jgen.writeEndArray();
 	    jgen.writeEndObject();
 		
 	}
 	
-	private void writeElement(JsonGenerator jgen, Value value) throws IOException {
-		jgen.writeArrayFieldStart("c");
-		
-		jgen.writeStartObject();
-		jgen.writeStringField("v", "Date("+value.getTime()+")");
-		jgen.writeEndObject();
-		
-		jgen.writeStartObject();
-		jgen.writeNumberField("v", value.getValue());
-		jgen.writeEndObject();
-		
-		jgen.writeStartObject();
-		jgen.writeStringField("v", getTooltip(value));
-		jgen.writeEndObject();		
-		
-		jgen.writeEndArray();
-	}
-
-	private String getTooltip(Value value) {
-		return "tooltip";
-	}
 
 }
