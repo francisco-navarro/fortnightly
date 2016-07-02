3a636f53a5fdeb4ba0dad874c8e32c69d97769ae
Paco Navarro <franciscojose.navarro.contractor@bbva.com>
2016-07-02 14:18:01 +0200

    Arrancar el webapp

--- back/es.fortnightly.back/src/main/java/es/fortnightly/back/resources/Team.java
+++ back/es.fortnightly.back/src/main/java/es/fortnightly/back/resources/Team.java
@@ -11,6 +11,20 @@ public class Team implements Serializable {
 	
 	private long id;
 	private String nombre;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 	
 	
 }
--- back/es.fortnightly.back/src/main/java/es/fortnightly/back/servlet/config/WebAppInitializer.java
+++ back/es.fortnightly.back/src/main/java/es/fortnightly/back/servlet/config/WebAppInitializer.java
@@ -1,18 +1,19 @@
 package es.fortnightly.back.servlet.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.ServletContext;  
+import javax.servlet.ServletException;  
 import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.WebApplicationInitializer;  
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
 import org.springframework.web.servlet.DispatcherServlet;  
   
 @Configuration
 public class WebAppInitializer implements WebApplicationInitializer {
 	
-
+	
+	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {  
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
         ctx.register(AppConfig.class);  
