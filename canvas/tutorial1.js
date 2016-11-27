f0a7e11801d9c1444fa66e5f70265c7a157328a7
Paco Navarro <packarbell@gmail.com>
2016-11-27 18:59:05 +0100

    Draw shapes

--- canvas/tutorial1.js
+++ canvas/tutorial1.js
@@ -1,17 +1,18 @@
-'use strict';
+/* eslint no-magic-numbers: 0 */
 
-var draw = function(){  
+var draw = function() {
+  'use strict';
   var canvas = document.getElementById('draw');
-  var ctx;  
+  var ctx;
 
   if (canvas.getContext){
     ctx = canvas.getContext('2d');
     ctx.fillStyle = 'rgb(200,0,0)';
-    ctx.fillRect (10, 10, 55, 50);
+    ctx.fillRect(10, 10, 55, 50);
+    ctx.clearRect(20, 20, 20, 20);
 
     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect (30, 30, 55, 50);
-
+    ctx.fillRect(30, 30, 55, 50);
   } else {
     console.warn('canvas not supported');
   }
