2158dea1bef7e5b9f87b595d2cfb2e18a8982715
Paco Navarro <packarbell@gmail.com>
2016-11-27 20:22:33 +0100

    Draw map tiles

--- canvas/tutorial1.js
+++ canvas/tutorial1.js
@@ -13,6 +13,30 @@ var draw = function() {
 
     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     ctx.fillRect(30, 30, 55, 50);
+
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fillStyle = 'rgba(161, 212, 144, 0.5)';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+    ctx.moveTo(110,75);
+    ctx.arc(75,75,35,0,Math.PI,false);   // Mouth (clockwise)
+    ctx.moveTo(65,65);
+    ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+    ctx.moveTo(95,65);
+    ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+    ctx.stroke();
+    //Triangle stroke
+    ctx.beginPath();
+    ctx.moveTo(125, 125);
+    ctx.lineTo(125, 45);
+    ctx.lineTo(45, 125);
+    ctx.closePath();
+    ctx.stroke();
   } else {
     console.warn('canvas not supported');
   }
