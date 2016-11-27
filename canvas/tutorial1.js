74224f1ae36c93777a8216a632cf80a98b494091
Paco Navarro <packarbell@gmail.com>
2016-11-27 18:47:02 +0100

    Draw shapes

---
+++ canvas/tutorial1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var draw = function(){  
+  var canvas = document.getElementById('draw');
+  var ctx;  
+
+  if (canvas.getContext){
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(200,0,0)';
+    ctx.fillRect (10, 10, 55, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect (30, 30, 55, 50);
+
+  } else {
+    console.warn('canvas not supported');
+  }
+};
