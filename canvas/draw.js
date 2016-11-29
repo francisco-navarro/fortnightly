c4f7601539e3e66876f455462e046e6cdc714706
Paco Navarro <packarbell@gmail.com>
2016-11-29 02:08:53 +0100

    Scenery in iso coordinates

--- canvas/draw.js
+++ canvas/draw.js
@@ -5,9 +5,33 @@ var app = {
 
     if (canvas.getContext) {
       app.ctx = canvas.getContext('2d');
-      app.map(10, 10);
+      app.ctx.font= '8px Georgia';
+
+      app.ctx.iLineTo = function(carX, carY) {
+        var isoX = carX + carY;
+        var isoY = (carY - carX) / 2.0;
+
+        app.ctx.lineTo(isoX, isoY);
+      };
+
+      app.ctx.iMoveTo = function(carX, carY) {
+        var isoX = carX + carY;
+        var isoY = (carY - carX) / 2.0;
+        app.ctx.moveTo(isoX, isoY);
+      };
+
+      app.map(9, 9);
+      app.scenery();
     } else {
       console.warn('canvas not compatible');
     }
   }
 };
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
--- canvas/map.js
+++ canvas/map.js
@@ -1,67 +1,55 @@
 /* eslint no-magic-numbers: 0 */
 app.map = function(height, width) {
   'use strict';
-  var SQUARE_SIZE = 64;
+  var map = this;
+  var SQUARE_SIZE = 32;
   var _60 = Math.PI / 3;
   var _30 = Math.PI / 6;
   //Calculamos la altura segun teorema del seno
-  var SQUARE_HEIGHT = 16;//Math.sin(_30) * (SQUARE_SIZE / 2) / Math.sin(_60);
+  var SQUARE_HEIGHT = 8; //Math.sin(_30) * (SQUARE_SIZE / 2) / Math.sin(_60);
 
-  function createMap(n, m) {
+  function init(n, m) {
     //Create a map of n*m positions
-    var offsetX = 280,
-      offsetY = SQUARE_SIZE;
-    
-    app.ctx.iLineTo = function() {
-      isoX = carX + carY;
-      isoY = carY - carX / 2.0;
-
-      carX = (isoX - isoY) / 1.5;
-      carY = isoX / 3.0 + isoY / 1.5;
-    };
+    map.offsetX = 300;
+    map.offsetY = 300;
+    map.height = n;
+    map.width = m;
 
     for(var i = 0; i < n; i++) {
       for(var j = 0; j < m; j++) {
         //http://gamedev.stackexchange.com/questions/34787/how-to-convert-mouse-coordinates-to-isometric-indexes
-        var x = i * SQUARE_SIZE/2 - j * SQUARE_SIZE/2 + offsetX;
-        var y = i * SQUARE_HEIGHT + j * SQUARE_HEIGHT + offsetY;
+        // var x = i * SQUARE_SIZE/2 - j * SQUARE_SIZE/2 + offsetX;
+        // var y = i * SQUARE_HEIGHT + j * SQUARE_HEIGHT + offsetY;
+        //mesh(x, y);
+        var x = i * SQUARE_SIZE - map.offsetX;
+        var y = j * SQUARE_SIZE + map.offsetY;
         mesh(x, y);
-        // mesh2(100 + i * SQUARE_SIZE, 100 + j * SQUARE_SIZE);
+        coordText(x + SQUARE_SIZE/2, y + SQUARE_SIZE/2, i, j);
         //http://www.alcove-games.com/advanced-tutorials/isometric-tile-picking/
       }
     }
   }
 
-  function mesh2(x, y){
-    app.ctx.moveTo(x, y);
+  function mesh(x, y){
+    app.ctx.iMoveTo(x, y);
     app.ctx.beginPath();
-    app.ctx.lineTo(x + SQUARE_SIZE, y);
-    app.ctx.lineTo(x + SQUARE_SIZE, y + SQUARE_SIZE);
-    app.ctx.lineTo(x, y + SQUARE_SIZE);
-    app.ctx.lineTo(x, y);
+    app.ctx.iLineTo(x + SQUARE_SIZE, y);
+    app.ctx.iLineTo(x + SQUARE_SIZE, y + SQUARE_SIZE);
+    app.ctx.iLineTo(x, y + SQUARE_SIZE);
+    app.ctx.iLineTo(x, y);
+    app.ctx.closePath();
     app.ctx.strokeStyle = '#19343A';
     app.ctx.stroke();
+    app.ctx.fillStyle = 'rgba(161, 212, 144, 0.4)';
+    // app.ctx.fill();
   }
 
-  function mesh(x, y) {
-    var B = [x + SQUARE_SIZE / 2, y + SQUARE_HEIGHT];
-    var C = [x + SQUARE_SIZE, y];
-    var D = [x + SQUARE_SIZE / 2, y - SQUARE_HEIGHT];
-
-    app.ctx.moveTo(x, y);
-    app.ctx.beginPath();
-    app.ctx.lineTo.apply(app.ctx, B);
-    app.ctx.lineTo.apply(app.ctx, C);
-    app.ctx.lineTo.apply(app.ctx, D);
-    app.ctx.lineTo(x, y);
-    app.ctx.closePath();
-    app.ctx.strokeStyle = '#09330A';
-    app.ctx.stroke();
-    //app.ctx.fillStyle = 'rgba(161, 212, 144, 0.8)';
-    //app.ctx.fill();
+  function coordText(carX, carY, n, m){
+    var isoX = carX + carY;
+    var isoY = (carY - carX) / 2.0;
+    app.ctx.fillText(n + ',' + m, isoX, isoY);
   }
 
-  createMap(height, width);
-  app.SQUARE_HEIGHT = SQUARE_HEIGHT;
-  app.SQUARE_SIZE = SQUARE_SIZE;
+  init(height, width);
+  map.SQUARE_SIZE = SQUARE_SIZE;
 };
